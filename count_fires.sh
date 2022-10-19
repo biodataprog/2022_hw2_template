b5ec7b307671a0ecc57efe7fbe862160e888215c
Jason Stajich <jasonstajich.phd@gmail.com>
2022-10-19 09:35:33 -0700

    FILE->FIRE

--- count_fires.sh
+++ count_fires.sh
@@ -13,15 +13,15 @@ echo "This report has the years: $MINYEAR-$MAXYEAR"
 # if you have problems the CSV file already part of this repository so you can use 'calfires_2021.csv'
 
 # print out the total number of fires (remember to remove the header line)
-TOTALFILECOUNT=0
+TOTALFIRECOUNT=0
 # put your code here to update this variable
-echo "Total number of calfire: $TOTALFILECOUNT"
+echo "Total number of calfire: $TOTALFIRECOUNT"
 
 # print out the number of fire in each year
 echo "Number of fires in each year follows:"
 
 
-# print out the name of the largest file use the GIS_ACRES and report the number of acres
+# print out the name of the largest fire use the GIS_ACRES and report the number of acres
 echo "Largest fire was $LARGEST and burned $LARGESTACRES"
 
 # print out the years - change the code in $(echo 1990) to print out the years (hint - how did you get MINYEAR and MAXYEAR?
