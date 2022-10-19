4e46695eadb0fa815f7f0b0cff48133b13eeb1b7
Jason Stajich <jasonstajich.phd@gmail.com>
2022-10-19 09:34:09 -0700

    typo from files->fires

--- count_fires.sh
+++ count_fires.sh
@@ -15,7 +15,7 @@ echo "This report has the years: $MINYEAR-$MAXYEAR"
 # print out the total number of fires (remember to remove the header line)
 TOTALFILECOUNT=0
 # put your code here to update this variable
-echo "Total number of files: $TOTALFILECOUNT"
+echo "Total number of calfire: $TOTALFILECOUNT"
 
 # print out the number of fire in each year
 echo "Number of fires in each year follows:"
